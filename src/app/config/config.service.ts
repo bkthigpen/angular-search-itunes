5a2ca2dafd5ab0f56ca9239cf6d25b4e39609753
Brandon Thigpen <brandonkthigpen@gmail.com>
2021-10-10 22:16:48 -0400

    Cleans up commented out code

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -6,12 +6,10 @@ import { map, catchError, retry } from 'rxjs/operators';
 @Injectable({ providedIn: 'root' })
 export class ConfigService {
   // base URL
-  // https://itunes.apple.com/search?term=jack+johnson&limit=25
   url: string = 'https://itunes.apple.com/search?term='
   constructor(private http: HttpClient) { }
 
   getResponse(term: string): Observable<any> {
-    // return this.http.get(this.url + term + '&limit=3');
     return this.http.get(`${this.url}${term}&limit=3`);
   }
 }
